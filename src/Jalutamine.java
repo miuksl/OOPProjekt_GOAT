19bb7c95b19ae550cfd6846f1d3597c168350ee1
miocristopher <miovahtra23@gmail.com>
2026-04-01 18:00:31 +0300

    aa

--- src/Jalutamine.java
+++ src/Jalutamine.java
@@ -1,2 +1,21 @@
-public class Jalutamine implements random_num{
+public class Jalutamine extends Treenimine{
+
+    private boolean kepikõnd;
+    private double distants;
+
+    public Jalutamine(int aeg) {
+        super(aeg);
+        this.kepikõnd = kepikõnd;
+    }
+
+    @Override
+    public String misIlmOn() {
+        return super.misIlmOn();
+    }
+
+    public void treening() {
+
+        int suvakas = suvalineNumber();
+        System.out.println(suvakas);
+    }
 }
